73f2d80d37c2be7c3da7d134b5db835685080723
Kinga Woch <kinga-niecko@o2.pl>
2023-03-21 14:53:53 +0100

    bookmark btn

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ toggleBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   toggleBtn.classList.remove("d-none");
 });
+
+const bookmarkBtn = document.querySelector(".bookmark-btn");
+const bookmarkBtnIcon = document.querySelector(".bookmark-btn-icon");
+
+bookmarkBtn.addEventListener("click", () => {
+  bookmarkBtn.classList.toggle("active");
+  bookmarkBtnIcon.classList.toggle("active");
+  if (bookmarkBtn.classList.contains("active")) {
+    bookmarkBtn.innerHTML = "Bookmarked";
+  } else {
+    bookmarkBtn.innerHTML = "Bookmark";
+  }
+});
+
+bookmarkBtnIcon.addEventListener("click", () => {
+  bookmarkBtn.classList.toggle("active");
+  bookmarkBtnIcon.classList.toggle("active");
+  if (bookmarkBtnIcon.classList.contains("active")) {
+    bookmarkBtn.innerHTML = "Bookmarked";
+  } else {
+    bookmarkBtn.innerHTML = "Bookmark";
+  }
+});
