96a0d92430821735fa243f139a600a70369272bf
Kinga Woch <kinga-niecko@o2.pl>
2023-03-22 09:47:50 +0100

    modal

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const toggleBtn = document.querySelector(".toggle-btn");
 const closeBtn = document.querySelector(".close-btn");
-const modal = document.getElementById("navModal");
-console.log(modal);
 
 toggleBtn.addEventListener("click", () => {
   toggleBtn.classList.add("d-none");
